03131078381470bacd623de4c61195d4148eb537
Edward Olsen <olsenedw@msu.edu>
2015-06-15 20:39:27 +0000

    WSGI integration. Starting Flask

---
+++ bin/brave.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+from random import randint
+from contextlib import closing
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+import pymysql
+
+# create our little application :)
+app = Flask(__name__, static_folder = "brave")
+app.config.from_object(__name__)
+app.debug=True
+
+
+def connect_db():
+	return pymysql.connect(host='localhost', user='ec2-user', passwd='m4ris4', db='bravery')
+
+@app.before_request
+def before_request():
+	g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+	db = getattr(g, 'db', None)
+	if db is not None:
+		db.close()
+
+@app.route('/')
+def show_entries():
+	cur = g.db.cursor()
+	cur.execute("SELECT * FROM champions")
+	champ_id_array = []
+	for i in range(cur.rowcount):
+		row = cur.fetchone()
+		champ_id_array.append(row[0])
+
+	rando = randint(0,len(champ_id_array))
+	sql = "SELECT * FROM champions WHERE id=%s;"
+	cur.execute(sql, (champ_id_array[rando]))
+
+	entries = [dict(title=row[1], text=row[2]) for row in cur.fetchall()]
+	return render_template('show_entries.html', entries=entries,rando=rando)
+@app.route('/images')
+def render_image(image_path):
+	return send_from_directory('',image_path)  
+
+
+
+if __name__ == '__main__':
+    app.run()
+
---
+++ bin/brave.wsgi
@@ -0,0 +1,4 @@
+import sys, os
+sys.path.append('/var/www/html/brave/bin')
+os.chdir("/var/www/html/brave/bin")
+from brave import app as application
