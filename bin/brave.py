122944ca4118f6500cb6d1518ec5757c5d202556
Edward Olsen <olsenedw@msu.edu>
2015-07-03 00:14:02 +0000

    Roll summoners

--- bin/brave.py
+++ bin/brave.py
@@ -30,10 +30,10 @@ def roll_items():
 	item_id_array = []
 	boot_id_array = []
 	final_build = []
-	jungle_param = "Enchantment%"
+	jungle_param = '"Enchantment%"'
 	jungle_item = False
 	jamble = re.compile("^Enchantment.*$")
-	cur.execute("SELECT * FROM items WHERE twisted_treeline=1 AND gold>=2000 AND (id != 3200 OR id != 3198 OR id != 3197 OR id != 3196);")
+	cur.execute("SELECT * FROM items WHERE twisted_treeline=1 AND gold>=2000 AND (id != 3200 AND id != 3198 AND id != 3197 AND id != 3196);")
 	for i in range(cur.rowcount):
 		row = cur.fetchone()
 		item_id_array.append(row[0])
@@ -50,33 +50,56 @@ def roll_items():
 	
 	### Add the rest of the items to build
 	for i in range(1,6):
+		print("Loop start"+ str(jungle_item))
 		rando = randint(0,len(item_id_array)-1)
-		if jungle_item == True:
-			sql = "SELECT * from items WHERE id=%s AND name NOT LIKE %s;"
-			cur.execute(sql, (item_id_array[rando], jungle_param))
-		elif jungle_item == False:
-			sql = "SELECT * FROM items WHERE id=%s;"
-			cur.execute(sql, (item_id_array[rando]))
-
-		check = cur.fetchone()
-		#print(check[1])
-		if jamble.match(check[1]): ### Aparently too stupid to use any() but regex is ok
-			if jungle_item == False:
-				jungle_item = True
-			elif jungle_item == True:
-				### Reroll until no jungle
-				jungle_item == True
-			sql = "SELECT * from items WHERE id=%s AND name NOT LIKE %s;"
-			cur.execute(sql, (item_id_array[rando], jungle_param))
 		sql = "SELECT * FROM items WHERE id=%s;"
 		cur.execute(sql, (item_id_array[rando]))
+		check = cur.fetchone()
+		### Check if rolled a jungle item
+		if jamble.match(check[1]): ### Aparently too stupid to use any() but regex is ok
+			### Found a jungle item
+			if jungle_item == True:
+				sql = "SELECT * from items WHERE id=%s AND name NOT LIKE %s;"
+				rando = randint(0,len(item_id_array)-1)
+				cur.execute(sql, (item_id_array[rando], jungle_param))
+				### Already have a jungle item
+				print("Already have" + str(jungle_item))
+			elif jungle_item == False:
+				sql = "SELECT * FROM items WHERE id=%s;"
+				cur.execute(sql, (item_id_array[rando]))
+				jungle_item = True
+				### First jungle item
+				print("First jungle"+ str(jungle_item))
+		else:
+			sql = "SELECT * FROM items WHERE id=%s;"
+			cur.execute(sql, (item_id_array[rando]))
+			### Not a jungle item
+			print("Not a jungle"+ str(jungle_item))
+
+		### Add item
+		print("Add item"+ str(jungle_item))
 		final_build.append([row[7] for row in cur.fetchall()])
 
 	return final_build
 
+def roll_summoners():
+	cur = g.db.cursor()
+	summoners = []
+	cur.execute("SELECT * FROM summoners WHERE twisted_treeline=1;")
+	summoner_id_array = []
+	for i in range(cur.rowcount):
+		row = cur.fetchone()
+		summoner_id_array.append(row[0])
+	for i in range(0,2):
+		rando = randint(0,len(summoner_id_array)-1)
+		sql = "SELECT * FROM summoners WHERE id=%s"
+		cur.execute(sql, (summoner_id_array[rando]))
+		summoners.append([row[6] for row in cur.fetchall()])
+	return summoners
+
 @app.route('/')
 def index():
-	### Roll Champion
+	### Roll Champion and skill
 	cur = g.db.cursor()
 	cur.execute("SELECT * FROM champions;")
 	champ_id_array = []
@@ -89,7 +112,13 @@ def index():
 	cur.execute(sql, (champ_id_array[rando]))
 
 	rando = randint(3,5)
-	entries = [dict(name=row[1], portrait=row[2], max_skill=row[rando],  passive=row[7]) for row in cur.fetchall()]
+	entries = [dict(name=row[1], portrait=row[2], max_skill=row[rando]) for row in cur.fetchall()]
+	
+	### Roll summoners
+	summoners = roll_summoners()
+
+	### Roll Items
+	final_build = roll_items()
 
 	### Roll Masteries
 	rando = randint(0,30)
@@ -99,10 +128,8 @@ def index():
 	ut_pts=30-(of_pts+df_pts)
 	masteries = [dict(offense=of_pts, defense=df_pts, utility=ut_pts)]
 
-	### Roll Items
-	final_build=roll_items()
 
-	return render_template('index.html', entries=entries, final_build=final_build, masteries=masteries)
+	return render_template('index.html', entries=entries, summoners=summoners, final_build=final_build, masteries=masteries)
 
 
 @app.route('/images')
